289169f267b1c8d84b570331a6e2c451bbb65b01
Oray Yılmaz <orayyilmaz16@gmail.com>
2024-12-03 16:17:12 +0300

    GET & POST methodu eğitimi tamamlandı

---
+++ forms/get.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php  
+    
+    if($_GET){
+      $query = $_GET['q'];
+      $category = $_GET['category'];
+
+      echo $query;
+      echo "<br>";
+      echo $category;
+    }
+?>
+
+<form  method="GET">
+    <input type="text"  name="q">
+    <input type="text"  name="category">
+    <button type="submit">Gönder</button>
+</form>
+    
+</body>
+</html>
